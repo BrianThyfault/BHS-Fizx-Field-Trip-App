fb25b124b3812d4b4f337425c5121d9b6e0e3483
BrianThyfault <student@bthyfault1.local>
2017-05-17 09:57:03 -0500

    Did stuff

--- GoogleFirebaseSignIn/MapViewController.swift
+++ GoogleFirebaseSignIn/MapViewController.swift
@@ -10,78 +10,23 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate
+class MapViewController: UIViewController, MKMapViewDelegate, UIScrollViewDelegate
 {
-    @IBAction func pageVCButton(_ sender: Any) {
-        performSegue(withIdentifier: "pageVCButton", sender: self)
-    }
     
-    @IBOutlet weak var myMapView: MKMapView!
-    let sixFlagsCoordiantes = CLLocationCoordinate2DMake(42.369680, -87.935544)
-    let sixFlagsDemon = CLLocationCoordinate2DMake(42.366378, -87.934907)
-    let sixFlagsSuperMan = CLLocationCoordinate2DMake(42.371061, -87.934164)
-    let sixFlagsRagingBull = CLLocationCoordinate2DMake(42.366195, -87.937116)
-    var locationManager = CLLocationManager.init()
-    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        locationManager.requestWhenInUseAuthorization()
-        
-        myMapView.delegate = self
-        addPinToMap()
-        goToCurrentLocation()
-        //centerMap(location: sixFlagsCoordiantes)
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 10
+        //scrollView.delegate = self
+        //scrollView.addSubview(imageView)
     }
     
-    func addPinToMap()
-    {
-        let myPlaza = MKPointAnnotation()
-        myPlaza.coordinate = sixFlagsCoordiantes
-        myPlaza.title = "6 flags"
-        myPlaza.subtitle = "Carousel Plaza"
-        myMapView.addAnnotation(myPlaza)
-        
-        let myDemon = MKPointAnnotation()
-        myDemon.coordinate = sixFlagsDemon
-        myDemon.title = "Demon"
-        myDemon.subtitle = "Questions 1-2"
-        myMapView.addAnnotation(myDemon)
-        
-        let mySuperMan = MKPointAnnotation()
-        mySuperMan.coordinate = sixFlagsSuperMan
-        mySuperMan.title = "SuperMan"
-        mySuperMan.subtitle = "Questions 3-4"
-        myMapView.addAnnotation(mySuperMan)
-        
-        let myRagingBull = MKPointAnnotation()
-        myRagingBull.coordinate = sixFlagsRagingBull
-        myRagingBull.title = "Raging Bull"
-        myRagingBull.subtitle = "Questions 5-6"
-        myMapView.addAnnotation(myRagingBull)
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
     }
-    
-    @IBAction func goToSixFlags(_ sender: Any)
-    {
-        centerMap(location: sixFlagsCoordiantes)
-    }
-    
-    
-    
-    func goToCurrentLocation()
-    {
-        let span = MKCoordinateSpan.init(latitudeDelta: 0.0075, longitudeDelta: 0.0075)
-        let region = MKCoordinateRegion.init(center: (locationManager.location?.coordinate)!, span: span)
-        myMapView.setRegion(region, animated: true)
-    }
-    
-    func centerMap(location: CLLocationCoordinate2D)
-    {
-        let mySpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        let myRegion = MKCoordinateRegion(center: location, span: mySpan)
-        myMapView.setRegion(myRegion, animated: true)
-    }
-    
-    
     
 }
